a4c9047fb2499244fd4f5edf44d5a913b9bb1c95
devdewa <uchiasasuke123.sp@gmail.com>
2024-01-04 18:50:34 +0700

    feat: use transformerDirectives & presetWebFonts

--- uno.config.ts
+++ uno.config.ts
@@ -1,12 +1,23 @@
-import { defineConfig, presetAttributify, presetUno } from 'unocss';
+import { defineConfig, presetAttributify, presetUno, presetWebFonts } from 'unocss';
+import transformerDirectives from '@unocss/transformer-directives';
 
 export default defineConfig({
   presets: [
     presetUno(),
     presetAttributify(),
+    presetWebFonts({
+      provider: 'bunny',
+      fonts: {
+        mono: 'DM Mono',
+      },
+    }),
   ],
   shortcuts: {
-    input: 'w-80 rounded border border-gray-500 p-1.5',
-    button: 'w-40 rounded border-none bg-gray-200 p-1.5',
+    input: 'rounded border-none ring ring-gray-100/70 bg-gray-50/50 p-3',
+    button: 'w-40 rounded border-none bg-yellow-200 p-2.5 cursor-pointer',
+    sidebar: 'px-12 bg-gray-50/50',
   },
+  transformers: [
+    transformerDirectives(),
+  ],
 });
