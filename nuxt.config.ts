f1f3a45a077dede546fe9e16dd6454a3cf8cac3a
devdewa <uchiasasuke123.sp@gmail.com>
2024-01-06 00:06:45 +0700

    feat: add nuxtjs/color-mode & iconify/json

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,11 @@
 export default defineNuxtConfig({
-  modules: ['@unocss/nuxt', '@pinia/nuxt', '@pinia-plugin-persistedstate/nuxt'],
+  modules: ['@unocss/nuxt', '@pinia/nuxt', '@pinia-plugin-persistedstate/nuxt', '@nuxtjs/color-mode'],
 
   pinia: {
     storesDirs: ['./stores/**'],
+  },
+  colorMode: {
+    classSuffix: '',
   },
   devtools: { enabled: false },
 });
