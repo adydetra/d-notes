75fc5c3899ab54369b3367aa8136900763a562f6
devdewa <uchiasasuke123.sp@gmail.com>
2024-01-06 01:15:36 +0700

    chore: toast effect on hover

--- components/Display.vue
+++ components/Display.vue
@@ -5,19 +5,31 @@ const props = defineProps({
   content: String,
   delete: Function,
 });
+const tooltipVisible = ref();
+
+function toggleTooltip() {
+  tooltipVisible.value = !tooltipVisible.value;
+}
 </script>
 
 <template>
   <section mb-6 rounded-xl bg-yellow-50 p-4 text-gray-600>
-    <h3 mt-0>
+    <h3 mt-0 leading-loose>
       {{ props.title }}
     </h3>
-    <p>{{ props.content }}</p>
+    <p text-sm leading-loose md:text-base>
+      {{ props.content }}
+    </p>
     <div betweenCenter>
-      <span text-xs>{{ props.date }}</span>
-      <button cursor-pointer rounded-md border-none bg-red-200 px-2 py-1 transition ease-in-out hover:bg-red-300 dark:text-black @click="props.delete?.(props.date)">
-        Delete
-      </button>
+      <span betweenCenter gap-1.5 text-xs><div i-ph:calendar-blank-duotone />{{ props.date }}</span>
+      <div relative>
+        <button aria-label="Remove" cursor-pointer rounded-md border-none bg-transparent p-0 transition ease-in-out dark:text-black @click="props.delete?.(props.date)" @mouseenter="toggleTooltip" @mouseleave="toggleTooltip">
+          <div i-lets-icons:trash-duotone text-2xl text-red-400 hover:text-red-600 />
+        </button>
+        <div v-show="tooltipVisible" class="-left-4 -top-7" absolute z-10 rounded bg-gray-700 px-2 py-1 text-xs text-white>
+          Remove
+        </div>
+      </div>
     </div>
   </section>
 </template>
