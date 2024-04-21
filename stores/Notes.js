53ef529339649134df19a0b1c17d337ab47b1bc5
devdewa <uchiasasuke123.sp@gmail.com>
2024-04-21 22:04:03 +0700

    chore: better default notes

--- stores/Notes.js
+++ stores/Notes.js
@@ -1,9 +1,14 @@
 export const useNotesStore = defineStore('note', () => {
-  const notes = ref([
-    { date: '', title: '', content: '' },
-  ]);
+  const notes = ref([]);
   const validationError = ref('');
-  const noteTitles = computed (() => notes.value.map(() => notes.title));
+  const noteTitles = computed (() => notes.value.map(note => note.title));
+
+  // Function to check if the notes array is empty
+  function isFirstVisit() {
+    return notes.value.length === 0;
+  }
+
+  // Function to add a new note
   function addNote(title, content) {
     const currentDate = new Date();
     const format = currentDate.toLocaleDateString('en-US', {
@@ -17,12 +22,18 @@ export const useNotesStore = defineStore('note', () => {
       content,
     });
   }
+
+  // Function to delete a note
   function deleteNote(date) {
     const index = notes.value.findIndex(note => note.date === date);
-
     if (index !== -1)
       notes.value.splice(index, 1);
   }
+
+  // If it's the first visit, add a default note
+  if (isFirstVisit())
+    addNote('Title is here', 'Content is here');
+
   return { notes, noteTitles, addNote, deleteNote, validationError };
 }, {
   persist: true,
