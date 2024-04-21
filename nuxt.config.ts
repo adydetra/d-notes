537977ff40878d18dba59b50da7b87bc6549a916
devdewa <uchiasasuke123.sp@gmail.com>
2024-04-21 19:42:16 +0700

    refactor: revision code

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,7 +16,13 @@ export default defineNuxtConfig({
       ],
     },
   },
-  modules: ['@unocss/nuxt', '@pinia/nuxt', '@pinia-plugin-persistedstate/nuxt', '@nuxtjs/color-mode'],
+  
+  modules: [
+    '@nuxtjs/color-mode',
+    '@pinia/nuxt',
+    '@pinia-plugin-persistedstate/nuxt',
+    '@unocss/nuxt',
+  ],
 
   pinia: {
     storesDirs: ['./stores/**'],
